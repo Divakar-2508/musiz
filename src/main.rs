51794c498ffd6452b7b53bf635f8573965580a70
Divakar-2508 <divakaraugust@gmail.com>
2023-12-28 15:38:15 +0530

    prev, next track impl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,71 @@
 pub mod music_player;
+use std::{io::Write, path::PathBuf};
+
 use music_player::MusicPlayer;
 
 fn main() {
     let mut player = MusicPlayer::new();
-    player.add("test.mp3".to_string());
-    player.add("test2.mp3".to_string());
-    player.remove(1);
-    player.play();
-    println!("Playing!!");
+    println!("Music Player Started");
+    let mut input = String::new();
+    loop {
+        input.clear();
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let args = input.split_whitespace().collect::<Vec<&str>>();
+        match args[0].trim() {
+            "add" => {
+                let song_name = String::from(args[1].trim()) + ".mp3";
+                let path = PathBuf::from(song_name);
+                if !path.is_file() {
+                    println!("Enter a valid song name");
+                } else {
+                    player.add(path);
+                }
+            },
+            "play" => {
+                if args.len() > 1 {
+                    player.play(2);
+                    continue;
+                } 
+                if let Some(value) = player.play(0) {
+                    println!("{}", value);
+                }
+            },
+            "prev" => {
+                if let Some(value) = player.prev() {
+                    println!("\nNow Playing: [{}] {}", value.0, value.1);
+                }
+            },
+            "pause" => player.pause(),
+            "next" => {
+                if let Some(value) = player.skip_track() {
+                    println!("\nNow Playing: [{}] {}",value.0, value.1);
+                }
+            },
+            "stop" => break,
+            "remove" => {
+                match args[1].parse::<usize>() {
+                    Ok(value) => {
+                        if player.len() > value {
+                            player.remove(value);
+                        } else {
+                            println!("Enter a valid index to remove");
+                        }
+                    }
+                    Err(_) => {
+                        println!("Enter a valid index to remove");
+                    }
+                }
+
+            },
+            "list" => {
+                for (index, song_name) in player.get_list().into_iter().enumerate() {
+                    println!("{}. {}",index+1, song_name);
+                }
+            }
+            _ => println!("Invalid command!"),
+        }
+    }
+    // println!("Playing!!");
 }
--- src/music_player.rs
+++ src/music_player.rs
@@ -1,12 +1,14 @@
-use std::{io::BufReader, fs::File, time::Duration};
+use std::{io::BufReader, fs::File, path::PathBuf};
 
 use rodio::{OutputStream, OutputStreamHandle, Sink};
 
+
 pub struct MusicPlayer {
-    queue: Vec<String>,
+    queue: Vec<PathBuf>,
     _stream: OutputStream,
     stream_handle: OutputStreamHandle,
     current_song: Sink,
+    index: i32,
 }
 
 impl MusicPlayer {
@@ -18,36 +20,83 @@ impl MusicPlayer {
             stream_handle,
             queue,
             current_song: Sink::new_idle().0,
+            index: 0,
         }
     }
 
-    pub fn add(&mut self, song: String) {
+    pub fn add(&mut self, song: PathBuf) {
         self.queue.push(song);
     }
 
-    pub fn play(&mut self) {
-        if self.queue.is_empty() {
-            return;
+    pub fn play(&mut self, mode: i32) -> Option<String> {
+        if mode == 0 {
+            if self.current_song.is_paused() {
+                self.resume();
+                return Some("Playing paused Song".to_string());
+            }
+    
+            if self.queue.is_empty() {
+                return  Some("Queue is Empty!" .to_string());
+            }
         }
-        let current_song = self.queue.remove(0);
-        let file = BufReader::new(File::open(current_song).unwrap());
-        self.current_song = self.stream_handle.play_once(file).unwrap();
-        std::thread::sleep(Duration::from_secs(10));
+        if mode == 2 {
+            
+        }
+        dbg!(self.get_list());
+        let current_song = self.queue[self.index as usize].clone();
+        if let Ok(file) = File::open(current_song) {
+            let file = BufReader::new(file);
+            self.current_song = self.stream_handle.play_once(file).unwrap();
+        }
+        return None;
+    }
+
+    pub fn resume(&self) {
+        self.current_song.play();
     }
 
     pub fn pause(&self) {
-
+        self.current_song.pause();
     }
 
     pub fn stop(&self) {
-        
+        self.current_song.stop();
     }
 
-    pub fn remove(&mut self, index: usize) -> Option<String> {
+    pub fn remove(&mut self, index: usize){
         if self.queue.len() > index-1 {
-            Some(self.queue.remove(index-1))
-        } else {
-            None
+            self.queue.remove(index-1);
         }
+    }
+
+    pub fn skip_track(&mut self) -> Option<(i32, String)> {
+        self.stop();
+        self.current_song.clear();
+        if !self.queue.len() < (self.index + 1) as usize {
+            return None;
+        }
+
+        self.index += 1;
+        self.play(1);
+        dbg!(self.index);
+        dbg!(self.get_list());
+        return Some((self.index + 1, self.queue[self.index as usize].to_string_lossy().to_string()));
+    }
+
+    pub fn prev(&mut self) -> Option<(i32, String)> {
+        if self.index == 0 {
+            return None;
+        }
+        self.index -= 1;
+        self.play(1);
+        return Some((self.index + 1, self.queue[self.index as usize].to_string_lossy().to_string()));
+    }
+
+    pub fn get_list(&self) -> Vec<String> {
+        self.queue.iter().map(|x| x.as_path().to_string_lossy().to_string()).collect()
+    }
+
+    pub fn len(&self) -> usize {
+        return self.queue.len();
     }
 }
